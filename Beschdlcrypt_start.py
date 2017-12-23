b75253bbab160c5a3a489e28d5af7a3daa2cf300
Beschdl <33841601+Beschdl@users.noreply.github.com>
2017-12-23 20:06:39 +0100

    0.0.1 alpha
    **Notes**:
     -Added MD5
     -Small Bug fix
     -Added encodings 
      -> Arranged auto-encoding

--- Beschdlcrypt_start.py
+++ Beschdlcrypt_start.py
@@ -1,20 +1,27 @@
 import sys
 import argparse
 import binascii
+import hashlib
 from binascii import b2a_hex
 from binascii import a2b_hex
 from base64 import b64decode, b64encode
 from base64 import b32decode, b32encode
 from math import exp, factorial, log, log10, log2, pi
 
+########## A free line
+print("\n")
 ########## Variables
 
 
 logo ="\n  _______\n <	 \ \n < 	  \          ________ \n < 	  /         /        \ \n <	  \        /     ____/ \n <	   \      <     / \n <         /      <     \____\n <	  /        \         \ \n <_______/   <>     \________/ <>	\n\n"
 
-version ="0.0.1a dev"
+version ="0.0.1a"
 
-algos = ["base64", "base32", "hex (hexadecimal)"]
+algos = ["base64", "base32", "hex (hexadecimal), md5"]
+
+
+encodings = ["UTF-8", "ASCII", "BIG5", "johab"]
+
 ######### Error functions
 
 def encDecErr():
@@ -26,10 +33,11 @@ def checkEncDec():
     if (args.encrypt) and (args.decrypt):
         print("ERROR: Please select either encryption or decryption method")
         sys.exit()
-    if (args.encrypt==0) and (args.decrypt==0) and not (args.input==None):
+    if (args.encrypt==0) and (args.decrypt==0) and not (args.input==None) and not (args.version) and not (args.logo) and (args.list==None):
         print("Please specify if you want to encrypt or decrypt ('--encrypt/--decrypt' or '-E/-D')")
         sys.exit()
     print("\n")
+
 
 
 ######### Command-Line Arguments
@@ -47,10 +55,10 @@ parser.add_argument('--encrypt', '-E', help ="Select encryption method", action=
 parser.add_argument('--decrypt', '-D', help ="Select decryption method", action="store_true")
 parser.add_argument('-type', '-t', type=int, 
                     help="Selectd Input method: \n 0: Raw Input \n 1: File Input")
-
+parser.add_argument('-encoding', '-e', 
+                    help="Select the encoding")
 parser.add_argument('-list', '-l',
                     help="List possibilities of arguments")
-
 
 parser.add_argument('-algorithm', '-a',
                     help="Select the algorithm")
@@ -65,19 +73,26 @@ parser.add_argument('-output', '-o',
 
 args = parser.parse_args()
 
+if (args.encoding==None):
+    args.encoding="ASCII"
 
-
-print("\n")
 
 
 ######### Command-Line Argument Functions
 
 
-if args.list=='algorithms':
+if args.list=="Algorithms" or args.list=="algos":
     print("   Availabe algorithms:\n")
-    for algosTemp in algos:
-        print(" - ", algosTemp)
-
+    for Temp in algos:
+        print(" - ", Temp)
+        
+elif args.list=="encodings":
+    print("   Availabe algorithms:\n")
+    for Temp in encodings:
+        print(" - ", Temp)
+    print("\nThese are not the only encodings, you can choose others, that are supported")
+elif not (args.list==None):
+    print("You did not select a valid list")
 
 checkEncDec()
 
@@ -107,7 +122,7 @@ class Algorithms:
             out = ""
             if (args.encrypt):
                 #### Here you need bytes
-                out = b64encode(bytes(args.input, 'ASCII'))
+                out = b64encode(bytes(args.input, args.encoding))
             elif (args.decrypt):
                 out = b64decode(args.input)
             else:
@@ -120,7 +135,7 @@ class Algorithms:
             out = ""
             if (args.encrypt):
                 #### Here you need bytes
-                out = b32encode(bytes(args.input, 'ASCII'))
+                out = b32encode(bytes(args.input, args.encoding))
             elif (args.decrypt):
                 out = b32decode(args.input)
             else:
@@ -132,16 +147,31 @@ class Algorithms:
         def hex():
             out = ""
             if (args.encrypt):
-                temp = bytes(args.input, 'ASCII')
+                temp = bytes(args.input, args.encoding)
                 out = b2a_hex(temp)
             elif (args.decrypt):
                 out = a2b_hex(args.input)
+            else:
+                encDecErr()
             return out
+            
 				
-
+    class OneWayFunctions:
+        
+        ### MessageDigest 5 (MD5)
+        def md5():      
+            if (args.encrypt):
+                m = hashlib.md5(args.input.encode(args.encoding))
+                out = m.hexdigest()
+            elif (args.decrypt):
+                print("ERROR: This is a OneWayFunction, you can only Encrypt stuff")
+            else:
+                encDecErr()
+            print(out)
+                
 a = Algorithms
 n = a.NonKeyEncryptions
-
+o = a.OneWayFunctions
 
 
 ###### This is the guy that switches between the functions
@@ -158,17 +188,18 @@ elif (args.algorithm=="base32"):
 elif (args.algorithm=="hex") or (args.algorithm=="hexadecimal"):
     outp = n.hex()
     print(outp)
-elif (args.algorithm==None):
-    print("Please specify an algorithm")
-else:
+elif (args.algorithm=="md5") or (args.algorithm=="MessageDigest5"):
+    o.md5()
+elif not (args.algorithm==None):
     print("Your algorithm is not valid, get a list of algorithms with 'Beschdlcrypt.py -list algorithms'; Did you type it wrong?")
 
 class Convert:
     def texttohex():
         if (args.input!="" and args.input!=1):
-            temp = bytes(args.input, 'ASCII')
+            temp = bytes(args.input, args.encoding)
             out = b2a_hex(temp)
             return out
         else:
             print ("ERROR: Please enter a valid string")
             sys.exit()
+            
